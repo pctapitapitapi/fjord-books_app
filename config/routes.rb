c291a73957679ea2ab51f24ce5cae5fbfea5f914
pctapitapitapi <emi.yamamoto73@gmail.com>
2022-03-11 15:52:45 +0900

    ユーザー一覧作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   get 'users/index'
   get 'users/show'
+  # resources :users, only: [:index, :show]
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
   resources :books
-  root to: 'books#index'
+  root to: 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
